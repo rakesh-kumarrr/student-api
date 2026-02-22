497f7f7858d572a1e703524a83b29280b7222c61
rakesh-kumarrr <rakeshkumarpanigrahi9437@gmail.com>
2026-02-22 19:45:43 +0530

    added unit tests

--- src/test/java/com/demo/student_api/StudentApiApplicationTests.java
+++ src/test/java/com/demo/student_api/StudentApiApplicationTests.java
@@ -1,13 +1,31 @@
 package com.demo.student_api;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class StudentApiApplicationTests {
+class StudentServiceTest {
+
+	@Autowired
+	private StudentService studentService;
 
 	@Test
-	void contextLoads() {
+	void testGetAllStudents() {
+		assertFalse(studentService.getAllStudents().isEmpty());
 	}
 
+	@Test
+	void testAddStudent() {
+		Student student = new Student(0, "Test User", "test@email.com");
+		Student added = studentService.addStudent(student);
+		assertNotNull(added);
+		assertEquals("Test User", added.getName());
+	}
+
+	@Test
+	void testDeleteStudent() {
+		boolean deleted = studentService.deleteStudent(1);
+		assertTrue(deleted);
+	}
 }
